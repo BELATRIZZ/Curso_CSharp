96334efc3d5e2f0530eab392135baf871787c0eb
BELATRIZZ <beatriz.sralcantara@senacsp.edu.br>
2025-01-28 22:25:37 -0300

    EXERCICIO DE CASA

---
+++ ConsoleApp- DATAS (VETOR)/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp__DATAS__VETOR_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string[] meses = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho,", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+            int[] dias = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+            for (int i = 0; i < meses.Length; i++)
+            {
+                Console.WriteLine("O mês de  "+ meses[i] + " tem " + dias [i] + " dias. ");
+
+            }
+            string[] semana = { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+
+            foreach (string dia in semana)
+            {
+                Console.WriteLine(dia);
+            }
+            */
+
+            /*
+            int[] numeros = { 10, 45, 22, 50, 6, -84, -2, 91, -23 };
+
+            Console.WriteLine(numeros.First());
+            Console.WriteLine(numeros.Last());
+            Console.WriteLine();
+            Console.WriteLine(numeros.Max());
+            Console.WriteLine(numeros.Min());
+            Console.WriteLine(numeros.Average());
+            */
+
+
+
+
+        }
+        string[] frutas = { "Morango", "Melancia", "Uva", " Maça", "Abacaxí", "Banana", "Ameixa", "Pitaya", "Melão", "Abacate" };
+        double[] Preços = { 5, 15, 8, 12, 10, 12, 9, 6, 13, 7 };
+
+        
+    }
+}
